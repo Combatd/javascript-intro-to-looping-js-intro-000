d3edaa45581d985dbb7d873e7ecacafb910baa21
Mark Calvelo <combatdstar@gmail.com>
2019-08-28 11:36:50 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,23 @@
+function forLoop(array) {
+  
+  for (let i = 0; i < 25; i++) {
+    var ele = array[i];
+    if (i == 1) { 
+        ele = `I am 1 strange loop.`;
+      } else {
+        ele = `I am ${i} strange loop.`;
+      }
+  }
+    
+  return array;
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n);
+    n -= 1
+  }
+  return "done";
+}
+
+function 
